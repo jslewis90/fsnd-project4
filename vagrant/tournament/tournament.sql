d95d6a40f87ddbb464eb61c437f2fbfd59db52a1
Jared Lewis <jslewis90@outlook.com>
2016-06-10 10:29:41 -0400

    Add files via upload

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,34 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Creates a fresh copy of the tournament database after dropping the current one
+\c vagrant
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
 
+
+-- Players Table
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+-- Matches Table
+CREATE TABLE matches (
+    id SERIAL PRIMARY KEY,
+    winner INTEGER REFERENCES players(id),
+    loser INTEGER REFERENCES players(id)
+);
+
+
+-- Standings View
+CREATE VIEW standings
+AS SELECT players.id as player, name,
+(SELECT count(matches.winner) FROM matches
+ WHERE players.id = matches.winner) as wins,
+(SELECT count(matches.id) FROM matches
+ WHERE players.id = matches.winner
+ OR players.id = matches.loser) as matches
+FROM players
+ORDER BY wins DESC, matches DESC;
